0ca2eb4aa4c0ccdc2a1ef11b583c6ec1375358aa
corneliucirlan <corneliucirlan@gmail.com>
2015-07-09 22:57:10 +0300

    Disabled auto-linking images to themselfs

--- php/functions/misc.php
+++ php/functions/misc.php
@@ -13,4 +13,14 @@
 		register_nav_menu('header-menu', __("Header Menu"));
 	});
 
+
+	/**
+	 * REMOVE IMAGE MEDIA LINKS
+	 */
+	add_action('admin_init', function() {
+    	$image_set = get_option( 'image_default_link_type' );
+    	if ($image_set !== 'none')
+        	update_option('image_default_link_type', 'none');
+	}, 10);
+
 ?>
