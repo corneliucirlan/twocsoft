951e261a58b0be76c6c49293690fffa30ea88b17
Corneliu Cîrlan <corneliu@corneliucirlan.com>
2016-11-18 22:25:23 +0200

    Added social media feed into blog posts

--- single.php
+++ single.php
@@ -9,7 +9,7 @@
 <?php
 
 	$cardSettings = array(
-		'cardWrapper'			=> 'col-xs-12',
+		'cardWrapper'			=> '',
 	);
 
 ?>
@@ -18,7 +18,19 @@
 <?php get_header(); ?>
 
 <div class="page-single row">
-	<?php renderCard($cardSettings) ?>
+	<main class="col-md-8">
+		<?php renderCard($cardSettings) ?>
+	</main>
+
+	<aside class="col-md-4">
+		<div class="card card-flat">
+			<div class="fb-page" data-href="https://www.facebook.com/corneliucirlan/" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-width="500" data-show-facepile="true"><blockquote cite="https://www.facebook.com/corneliucirlan/" class="fb-xfbml-parse-ignore"><a class="text-center" href="https://www.facebook.com/corneliucirlan/"><?php bloginfo('name') ?> on Facebook</a></blockquote></div>
+		</div>
+
+		<div class="card card-flat">
+			<a class="twitter-timeline" data-height="500" href="https://twitter.com/corneliucirlan"><?php bloginfo('name') ?> on Twittern</a>
+		</div>
+	</aside>
 </div>
 
 <?php get_footer(); ?>
