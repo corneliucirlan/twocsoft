1988f9210ff355bba44e1e7ae1c84e969e051079
Corneliu Cîrlan <corneliu@corneliucirlan.com>
2017-02-03 16:36:28 +0200

    Disabled RSS Feed

--- functions/remove.php
+++ functions/remove.php
@@ -40,4 +40,18 @@
 	remove_action('wp_head', 'print_emoji_detection_script', 7);
 	remove_action('wp_print_styles', 'print_emoji_styles');
 
+	// Remove RSS Feed
+	function wpb_disable_feed()
+	{
+		wp_die( __('No feed available, please visit our <a href="'. get_bloginfo('url') .'">homepage</a>!') );
+	}
+
+	add_action('do_feed', 'wpb_disable_feed', 1);
+	add_action('do_feed_rdf', 'wpb_disable_feed', 1);
+	add_action('do_feed_rss', 'wpb_disable_feed', 1);
+	add_action('do_feed_rss2', 'wpb_disable_feed', 1);
+	add_action('do_feed_atom', 'wpb_disable_feed', 1);
+	add_action('do_feed_rss2_comments', 'wpb_disable_feed', 1);
+	add_action('do_feed_atom_comments', 'wpb_disable_feed', 1);
+
 ?>
