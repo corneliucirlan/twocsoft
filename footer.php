2e78b469847b561616d713541d8f5e7e811607ee
Corneliu Cîrlan <corneliu@corneliucirlan.com>
2019-01-29 16:54:07 +0200

    Use site description on footer

--- footer.php
+++ footer.php
@@ -21,7 +21,7 @@
                 <div class="row">
                     <div class="brand col-12 col-md-6 col-lg-4">
                         <img class="footer-logo" src="<?php echo get_template_directory_uri() ?>/assets/img/footer-logo.png" alt="<?php bloginfo('title') ?>" />
-                        <h5 class="footer-title">Freelance Web Developer | UX/UI Designer</h5>
+                        <h5 class="footer-title"><?php bloginfo('description') ?></h5>
                     </div>
 
                     <div class="contact col-12 col-md-6 col-lg-4">
