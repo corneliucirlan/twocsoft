b709b5e844c1b7ba2d4a8574b7eb17db6fafde8d
Corneliu Cîrlan <corneliu@corneliucirlan.com>
2019-05-14 22:55:23 +0300

    Updated frontpage HTML

--- front-page.php
+++ front-page.php
@@ -15,18 +15,18 @@
 
 <?php get_header(); ?>
 
-<div class="home-services row">
+<section class="home-services row">
 	<?php for ($counter = 1; $counter <= 5; $counter++): ?>
-		<div class="home-service col">
+		<article class="home-service col">
 			<i class="service-icon fa-4x fa-<?php echo get_field('home_service_icon_'.$counter) ?>"></i>
 			<h4 class="service-title"><?php echo get_field('home_service_title_'.$counter) ?></h4>
 			<p class="service-text"><?php echo get_field('home_service_text_'.$counter) ?></p>
-		</div>
+		</article>
 	<?php endfor; ?>
-</div>
+</section>
 
 <h2 class="home-blog"><?php _e('Latest blog entries', 'cornelius') ?></h2>
-<div class="latest-posts row">
+<section class="latest-posts row">
 
 	<?php $latestBlogEntries = new WP_Query(array('post_type' => 'post', 'posts_per_page' => 4)); ?>
 
@@ -34,14 +34,14 @@
 
 		<?php while ($latestBlogEntries->have_posts()): $latestBlogEntries->the_post(); ?>
 
-			<div class="latest-post col-xs-12 col-md-6 col-lg-3">
+			<article class="latest-post col-xs-12 col-md-6 col-lg-3">
 				<a aria-label="Read more on <?php the_title() ?>" href="<?php the_permalink() ?>"><?php the_post_thumbnail('thumbnail') ?></a>
 				<a aria-label="Read more on <?php the_title() ?>" href="<?php the_permalink() ?>"><h4 class="latest-title"><?php the_title() ?></h4></a>
-			</div>
+			</article>
 
 		<?php endwhile; ?>
 
 	<?php endif; ?>
-</div>
+</section>
 
 <?php get_footer(); ?>
